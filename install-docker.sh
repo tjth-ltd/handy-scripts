32efc1465c42dae06cae08cb178d923d9d0a2249
Tim Harrison <tim@tjth.co>
2018-07-23 23:17:06 +0100

    Install Docker Ubuntu

---
+++ install-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Docker Install
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+# Docker-Compose Install
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
